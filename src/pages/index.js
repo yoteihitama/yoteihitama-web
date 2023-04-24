8d785e98c5fc8d6d5976b2323205bbb61dacbd9a
yoteihitama <131521354+yoteihitama@users.noreply.github.com>
2023-04-25 00:33:29 +0900

    feat: add title and meta tags

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,19 @@
 import UrlEncoderDecoder from "@/components/UrlEncoderDecoder";
-import { Container, Grid } from "@mui/material";
+import { Container } from "@mui/material";
+import Head from "next/head";
 
 function HomePage() {
     return (
-        <Container maxWidth="lg">
-            <UrlEncoderDecoder />
-        </Container>
+        <>
+            <Head>
+                <title>{process.env.NEXT_PUBLIC_URL_ENCODER_PAGE_TITLE}</title>
+                <meta name="description" content={process.env.NEXT_PUBLIC_URL_ENCODER_PAGE_DESCRIPTION} />
+                <meta name="keywords" content={process.env.NEXT_PUBLIC_URL_ENCODER_PAGE_KEYWORDS} />
+            </Head>
+            <Container maxWidth="lg">
+                <UrlEncoderDecoder />
+            </Container>
+        </>
     );
 }
 
